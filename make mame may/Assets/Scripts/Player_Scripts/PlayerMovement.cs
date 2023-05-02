1c82308fe22679eb23381b94e1a826345fb01492
Jonaslim201 <jonaslimwx@gmail.com>
2023-05-02 13:47:28 +0800

    WallJump, Flip bug still not fixed

--- make mame may/Assets/Scripts/Player_Scripts/PlayerMovement.cs
+++ make mame may/Assets/Scripts/Player_Scripts/PlayerMovement.cs
@@ -22,6 +22,22 @@ public class PlayerMovement : MonoBehaviour
     bool isDashing = false;
     float distanceTraveled = 0f;
 
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    private bool isWallDetected;
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(3f, 5f);
+
     // Update is called once per frame
     void Update()
     {
@@ -51,6 +67,8 @@ public class PlayerMovement : MonoBehaviour
                 isDashing = true;
             }
         }
+        WallSlide();
+        WallJump();
     }
 
     public void OnLanding()
@@ -66,8 +84,10 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         // Move character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
+        if(!isWallJumping){
+            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+            jump = false;
+        }
 
         if (isDashing)
         {
@@ -89,5 +109,60 @@ public class PlayerMovement : MonoBehaviour
         {
             remainingDashCooldown -= Time.fixedDeltaTime;
         }
+    }
+
+    private bool IsWalled(){
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private bool IsGrounded(){
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void WallSlide(){
+        if (IsWalled() && !IsGrounded() && horizontalMove != 0f)
+        {
+            isWallSliding = true;
+            rb2D.velocity = new Vector2(rb2D.velocity.x, Mathf.Clamp(rb2D.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else{
+            isWallSliding = false;
+        }
+    }
+
+    private void WallJump(){
+        if (isWallSliding){
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else{
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f){
+            isWallJumping = true;
+            rb2D.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection){
+                Flip();
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping(){
+        isWallJumping = false;
+        Flip();
+    }
+
+    private void Flip(){
+        Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
     }
 }
